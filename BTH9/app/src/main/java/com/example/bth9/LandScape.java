33e1c8ffcf2af2deebf00c3d24bb4242e108aefc
trantrunghung-19 <trantrunghung000@gmail.com>
2026-03-27 15:57:01 +0700

    Create LandScape.java

---
+++ BTH9/app/src/main/java/com/example/bth9/LandScape.java
@@ -0,0 +1,27 @@
+package com.example.bth9;
+
+public class LandScape {
+    String landImageFileName;
+    String landCation;
+
+    public LandScape(String landImageFileName, String landCation) {
+        this.landImageFileName = landImageFileName;
+        this.landCation = landCation;
+    }
+
+    public String getLandCation() {
+        return landCation;
+    }
+
+    public void setLandCation(String landCation) {
+        this.landCation = landCation;
+    }
+
+    public String getLandImageFileName() {
+        return landImageFileName;
+    }
+
+    public void setLandImageFileName(String landImageFileName) {
+        this.landImageFileName = landImageFileName;
+    }
+}
