b2c349c0e605820bda4da8df59b6321bf7620d3a
trantrunghung-19 <trantrunghung000@gmail.com>
2026-03-27 15:56:59 +0700

    Create LandScapeAdapter.java

---
+++ BTH9/app/src/main/java/com/example/bth9/LandScapeAdapter.java
@@ -0,0 +1,73 @@
+package com.example.bth9;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> lstData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom=LayoutInflater.from(context);
+        View vItem=cai_bom.inflate(R.layout.item_land,parent,false);
+        ItemLandHolder viewholderCreated=new ItemLandHolder(vItem);
+        return viewholderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        LandScape landScapeHienThi= lstData.get(position);
+        String caption = landScapeHienThi.getLandCation();
+        String tenFileAnh= landScapeHienThi.getLandImageFileName();
+        holder.tvCaption.setText(caption);
+            String packageName=holder.itemView.getContext().getPackageName();
+            int imageID=holder.itemView.getResources().getIdentifier(tenFileAnh,"mipmap",packageName);
+        holder.ivLandscape.setImageResource(imageID);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstData.size();
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
+        TextView tvCaption;
+        ImageView ivLandscape;
+
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption=itemView.findViewById(R.id.textViewCation);
+            ivLandscape=itemView.findViewById(R.id.imageViewLand);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int viTriDuocClick= getAdapterPosition();
+            LandScape phanTuDuocClick=lstData.get(viTriDuocClick);
+            String ten= phanTuDuocClick.getLandCation();
+            String tenFile= phanTuDuocClick.getLandImageFileName();
+            String chuoiThongBao="Ban vua click vao:"+ten;
+            Toast.makeText(v.getContext(), chuoiThongBao, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+}
