e8d070a1f0fc7a746b8d77f02a28c3179d470f10
trantrunghung-19 <trantrunghung000@gmail.com>
2026-04-20 08:06:30 +0700

    lan 4

--- ThiGK2TranTrungHung/app/src/main/java/com/example/thigk2trantrunghung/ThanhPhoFragment.java
+++ ThiGK2TranTrungHung/app/src/main/java/com/example/thigk2trantrunghung/ThanhPhoFragment.java
@@ -2,63 +2,51 @@ package com.example.thigk2trantrunghung;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ThanhPhoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ThanhPhoFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ThanhPhoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ThanhPhoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ThanhPhoFragment newInstance(String param1, String param2) {
-        ThanhPhoFragment fragment = new ThanhPhoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recylerViewDatas;
+    RecyclerView recyclerViewLandscape;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        recylerViewDatas = getDataForRecyclerView();
+        recyclerViewLandscape=findViewById(R.id.recyclerLand);
+        //RecyclerView.LayoutManager layoutLinear= new LinearLayoutManager(this);
+        //recyclerViewLandscape.setLayoutManager(layoutLinear);
+        //RecyclerView.LayoutManager layoutLinearHorizonal= new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        //recyclerViewLandscape.setLayoutManager(layoutLinearHorizonal);
+        RecyclerView.LayoutManager layoutGrid= new GridLayoutManager(this,2);
+        recyclerViewLandscape.setLayoutManager(layoutGrid);
+        landScapeAdapter= new LandScapeAdapter(this, recylerViewDatas);
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thanh_pho, container, false);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu= new ArrayList<LandScape>();
+        LandScape landScape1= new LandScape("","");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("",""));
+        dsDuLieu.add(new LandScape("",""));
+        dsDuLieu.add(new LandScape("",""));
+        return dsDuLieu;
     }
 }
